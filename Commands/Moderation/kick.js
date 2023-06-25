3cde3be0e3e4e787a2d2a6e7e173ffa5c9ebc672
kurya <kurennov15@gmail.com>
2023-06-25 15:39:04 +0300

    main

--- Commands/Moderation/kick.js
+++ Commands/Moderation/kick.js
@@ -1,13 +1,9 @@
-const { SlashCommandBuilder, EmbedBuilder, Client } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('kick')
         .setDescription('Исключает пользователя из сервера')
-<<<<<<< HEAD
-        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
-=======
->>>>>>> 5984783 (main)
         .addUserOption((option) =>
             option.setName('user')
                 .setDescription('Пользователь')
@@ -32,10 +28,6 @@ module.exports = {
                 });
             }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d570a12 (main)
             if (member.id === interaction.guild.ownerId) {
                 return interaction.reply({
                     content: 'Исключить овнера? Гений...',
@@ -43,33 +35,18 @@ module.exports = {
                 });
             }
 
-            /* if (member.id === client.id) {
+            if (member.id === client.user.id) {
                 return interaction.reply({
                     content: 'Бот не может исключить самого себя...',
                     ephemeral: true
                 });
-            } */
+            }
 
             const embedError = new EmbedBuilder()
                 .setColor('#cdd6f4')
                 .setDescription(`Невозможно исключить пользователя "${user.username}", так как его роль выше вашей`)
                 .setFooter({
                     text: `Вызвал: ${interaction.user.username}`,
-=======
-=======
-            } */
-
->>>>>>> d570a12 (main)
-            const embedError = new EmbedBuilder()
-                .setColor('#cdd6f4')
-                .setDescription(`Невозможно исключить пользователя "${user.username}", так как его роль выше вашей`)
-                .setFooter({
-<<<<<<< HEAD
-                    text: `Вызвал: ${interaction.author.username}`,
->>>>>>> 5984783 (main)
-=======
-                    text: `Вызвал: ${interaction.user.username}`,
->>>>>>> d570a12 (main)
                     iconURL: user.displayAvatarURL({ dinamic: true, size: 4096 }),
                 })
                 .setTimestamp();
@@ -82,21 +59,11 @@ module.exports = {
             }
 
             const embedSucess = new EmbedBuilder()
-<<<<<<< HEAD
-<<<<<<< HEAD
                 .setColor('#cdd6f4')
-                .setDescription(`Пользователь "${user.username}" успешно исключен`)
-                .setFooter({
-                    text: `Вызвал: ${interaction.user.username}`,
-=======
-                .setColor('#2B2D31')
-=======
-                .setColor('#cdd6f4')
->>>>>>> d570a12 (main)
                 .setDescription(`Пользователь "${user.username}" успешно исключен`)
                 .setFooter({
                     text: `Вызвал: ${interaction.author.username}`,
-                    iconURL: user.displayAvatarURL({ dinamic: true, size: 4096 }),
+                    iconURL: user.displayAvatarURL({ dinamic: true, size: 4096 })
                 })
                 .setTimestamp();
 
@@ -105,6 +72,6 @@ module.exports = {
             return interaction.reply({
                 embeds: [embedSucess],
                 ephemeral: true
-            })
+            });
         }
 }
